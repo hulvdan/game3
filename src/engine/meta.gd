3ffb83be4674002ad6e3fa3cca36cf05afef3815
hulvdan <Hulvdan@yandex.ru>
2026-02-01 16:07:30 +0500

    Update meta.gd

--- src/engine/meta.gd
+++ src/engine/meta.gd
@@ -3,6 +3,50 @@ extends Node
 @export var metrics: MetricsBase
 @export var platform: PlatformBase
 
+var _async_data_loaded = false
+
+
+func _on_request_completed(
+		_result: int,
+		response_code: int,
+		_headers: PackedStringArray,
+		body: PackedByteArray,
+) -> void:
+	if response_code != 200:
+		printerr('Failed to download async_data.pck')
+		return
+
+	var file = FileAccess.open("user://async_data.pck", FileAccess.WRITE)
+	if not file:
+		printerr('Failed to open async_data.pck for writing')
+		return
+
+	var stored: bool = file.store_buffer(body)
+	file.close()
+
+	if not stored:
+		printerr('Failed to write to async_data.pck')
+		return
+
+	_async_data_loaded = ProjectSettings.load_resource_pack("user://async_data.pck")
+	if _async_data_loaded:
+		print('Loaded async_data.pck', _async_data_loaded)
+	else:
+		printerr('Failed to load async_data.pck')
+
+
+func _ready() -> void:
+	if OS.has_feature('web'):
+		var http: HTTPRequest = HTTPRequest.new()
+
+		add_child(http)
+
+		@warning_ignore("unsafe_method_access")
+		http.request_completed.connect(_on_request_completed)
+
+		var origin: String = JavaScriptBridge.eval("window.location.origin")
+		http.request(origin + "/async_data.pck")
+
 
 func metric(id: String) -> void:
 	metrics.metric(id)
