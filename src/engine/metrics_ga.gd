c8c5f561a88e2054e258860caeb8a11996768ba8
hulvdan <Hulvdan@yandex.ru>
2026-02-07 13:16:03 +0500

    Update metrics_ga.gd

--- src/engine/metrics_ga.gd
+++ src/engine/metrics_ga.gd
@@ -13,18 +13,7 @@ var _js_window = null
 func _ready() -> void:
 	if OS.has_feature('web'):
 		_js_window = JavaScriptBridge.get_interface("window")
-
-		# _js_window.GameAnalytics("setEnabledInfoLog", true)
-		# _js_window.GameAnalytics("setEnabledVerboseLog", true)
-
 		_js_window.GameAnalytics("configureBuild", CodegenVersion.VERSION)
-
-		# _js_window.GameAnalytics("configureAvailableResourceCurrencies", ["gems", "gold"])
-		# _js_window.GameAnalytics("configureAvailableResourceItemTypes", ["boost", "gold"])
-		# _js_window.GameAnalytics("configureAvailableCustomDimensions01", ["ninja", "samurai"])
-		# _js_window.GameAnalytics("configureAvailableCustomDimensions02", ["whale", "dolphin"])
-		# _js_window.GameAnalytics("configureAvailableCustomDimensions03", ["horde", "alliance"])
-
 		_js_window.GameAnalytics("initialize", game_id, game_secret)
 
 
