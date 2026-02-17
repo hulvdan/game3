678b84af80dfc92118241f18490f8f9343b42634
hulvdan <Hulvdan@yandex.ru>
2026-02-17 09:26:00 +0500

    upd

--- src/game/bar.gd
+++ src/game/bar.gd
@@ -4,9 +4,10 @@ class_name Bar
 
 @export var margin: int = 4
 
+var fade: bool = false
+
 @onready var _bar: ColorRect = %_bar
 @onready var _margin_container: MarginContainer = %_margin_container
-var _fade: bool = false
 
 
 func _ready() -> void:
@@ -14,15 +15,15 @@ func _ready() -> void:
 		_margin_container.add_theme_constant_override(side, margin)
 
 
-func init(color: Color, fade: bool) -> void:
-	_fade = fade
+func init(color: Color, fade_: bool) -> void:
+	fade = fade_
 	_bar.color = color
 
 
 func set_progress(p: float) -> void:
 	assert(p >= 0)
 	assert(p <= 1)
-	if _fade:
+	if fade:
 		if p < 1:
 			_bar.color.a = 0.4
 		else:
