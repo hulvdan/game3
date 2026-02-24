da7d0070a99a303649a977572c2ef427e4d8bd82
hulvdan <Hulvdan@yandex.ru>
2026-02-24 19:14:36 +0500

    upd

--- src/game/ai_action_chase.gd
+++ src/game/ai_action_chase.gd
@@ -3,11 +3,11 @@ extends ActionLeaf
 
 class_name ActionChase
 
-var _attack_distance_sqr: float
+var _attack_distance: float
 
 
 func set_attack_distance(value: float) -> void: ##
-	_attack_distance_sqr = value * value
+	_attack_distance = value
 ##
 
 
@@ -17,7 +17,11 @@ func tick(actor_: Node, _blackboard: Blackboard) -> int:
 	var pos = bf.from_xz((actor as Node3D).transform.origin)
 	var dpos: Vector2 = target_pos - pos
 
-	if dpos.length_squared() <= _attack_distance_sqr:
+	if glib.v.get_debug_collisions():
+		ImmediateGizmos3D.set_transform(actor.transform)
+		ImmediateGizmos3D.line_circle(Vector3(0, 0, 0), Vector3(0, 1, 0), _attack_distance)
+
+	if dpos.length_squared() <= _attack_distance * _attack_distance:
 		# Target is in attack range.
 		actor.controller.move = Vector2(0, 0)
 		return SUCCESS
