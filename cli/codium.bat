f7ebac3a8e701546ad470dbec740ff1cddb30d35
hulvdan <Hulvdan@yandex.ru>
2026-03-04 17:34:33 +0500

    f

--- cli/codium.bat
+++ cli/codium.bat
@@ -4,3 +4,4 @@ call "c:\Users\user\dev\home\emsdk\emsdk.bat" activate latest
 call "c:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 set EMSCRIPTEN=c:\Users\user\dev\home\emsdk\upstream\emscripten
 codium .
+exit
