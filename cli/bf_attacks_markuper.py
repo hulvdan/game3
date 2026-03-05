8d5b285ae6307de0517fd4379efd4378b9d904ac
hulvdan <Hulvdan@yandex.ru>
2026-03-05 13:12:27 +0500

    markuper: my own tabs

--- cli/bf_attacks_markuper.py
+++ cli/bf_attacks_markuper.py
@@ -66,55 +66,57 @@ def v2sub(v1: tuple[t.Any, t.Any], v2: tuple[t.Any, t.Any]) -> tuple[t.Any, t.An
   return (v1[0] - v2[0], v1[1] - v2[1])
 
 
+def panel_visualizer() -> None:  ##
+  pass
+  ##
+
+
+def panel_explorer() -> None:  ##
+  pass
+  ##
+
+
+def panel_timeline() -> None:  ##
+  hovered_line = -1
+  for field_index, field in enumerate(("scale", "offset", "rotation")):
+    color = (1, 1, 0, 1) if (field_index == g.timeline_hovered_line) else (1, 1, 1, 1)
+    imgui.text_colored(color, field)
+    if imgui.is_item_hovered():
+      hovered_line = field_index
+  g.timeline_hovered_line = hovered_line
+  ##
+
+
 # def _frame() -> None:  ##
 #   im.begin("Visualization")
 #   im.end()
-
 #   im.begin("Explorer")
 #   im.end()
-
 #   im.begin("Timeline")
-
-#   hovered_line = -1
-
 #   draw_list = im.get_window_draw_list()
-
 #   pos = im.get_cursor_screen_pos()
 #   size = v2sub(im.get_window_size(), v2sub(pos, im.get_window_pos()))
-
 #   draw_list.add_line(pos, v2add(pos, size), im.color_convert_float4_to_u32((1, 0, 0, 1)))
-
-#   for field_index, field in enumerate(("scale", "offset", "rotation")):
-#     color = (1, 1, 0, 1) if (field_index == g.timeline_hovered_line) else (1, 1, 1, 1)
-#     im.text_colored(color, field)
-#     if im.is_item_hovered():
-#       hovered_line = field_index
-
 #   # # Draw keyframes as small circles
 #   # for kf in g.timeline:
 #   #   x = pos[0] + (kf.index / 5.0) * timeline_width
 #   #   y = pos[1] + timeline_height / 2
 #   #   draw_list.add_circle_filled((x, y), 5, im.color_convert_float4_to_u32((1, 0, 0, 1)))
-
 #   im.end()
-
-#   g.timeline_hovered_line = hovered_line
 #   ##
 
 
 @command
 def tool_attacks_markuper() -> None:  ##
-  # print(
-  #   f"For information, demos sources are available in {demo_utils.api_demos.demos_python_folder()}"
-  # )
+  print(
+    f"For information, demos sources are available in {demo_utils.demos_assets_folder()}"
+  )
 
   # Part 1: Define the runner params
 
   runner_params = hello_imgui.RunnerParams()
   runner_params.app_window_params.window_title = "Attacks Markuper"
-  runner_params.app_window_params.window_geometry.window_size_state = (
-    hello_imgui.WindowSizeState.maximized
-  )
+  runner_params.app_window_params.restore_previous_geometry = True
 
   # Menu bar
   runner_params.imgui_window_params.show_menu_bar = True
@@ -205,6 +207,17 @@ def tool_attacks_markuper() -> None:  ##
     window.gui_function = make_group_fn(group)
     dockable_windows.append(window)
 
+  for panel_label, panel_function in (
+    ("Explorer", panel_explorer),
+    ("Visualizer", panel_visualizer),
+    ("Timeline", panel_timeline),
+  ):
+    window = hello_imgui.DockableWindow()
+    window.label = panel_label
+    window.dock_space_name = "MainDockSpace"
+    window.gui_function = panel_function
+    dockable_windows.append(window)
+
   runner_params.docking_params.dockable_windows = dockable_windows
 
   # the main gui is only responsible to give focus to ImGui Bundle dockable window
@@ -225,11 +238,6 @@ def tool_attacks_markuper() -> None:  ##
 
   runner_params.callbacks.show_gui = show_gui
 
-  if "test_engine" in dir(
-    imgui
-  ):  # only enable test engine if available (i.e. if imgui bundle was compiled with it)
-    runner_params.use_imgui_test_engine = True
-
   def setup_imgui_config() -> None:
     imgui.get_io().config_flags |= imgui.ConfigFlags_.nav_enable_keyboard.value
 
@@ -244,66 +252,6 @@ def tool_attacks_markuper() -> None:  ##
   addons.with_im_anim = True
   immapp.run(runner_params=runner_params, add_ons_params=addons)
   ##
-
-
-# def show_imgui(
-#   frame: t.Callable[[], None], key_callback, should_exit: t.Callable[[], bool]
-# ):  ##
-#   glfw.init()
-#   glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
-#   glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
-#   glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, glfw.TRUE)
-#   glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
-#   glfw.window_hint(glfw.VISIBLE, True)
-#   window = glfw.create_window(
-#     width=1600, height=900, title="Attacks Markuper", monitor=None, share=None
-#   )
-#   glfw.maximize_window(window)
-#   glfw.make_context_current(window)
-#   im.create_context()
-#   io = im.get_io()
-#   io.config_flags |= ConfigFlags.NAV_ENABLE_KEYBOARD
-#   io.config_flags |= ConfigFlags.DOCKING_ENABLE
-#   io.config_flags |= ConfigFlags.VIEWPORTS_ENABLE
-
-#   with open(bf.PROJECT_DIR / "cli" / "ComicCode-Semibold.ttf", "rb") as f:
-#     font_data = f.read()
-#   font = io.fonts.add_font_from_memory_ttf(font_data, 32)
-#   renderer = GlfwRenderer(window, prev_key_callback=key_callback)
-
-#   while not (glfw.window_should_close(window) or should_exit()):
-#     glfw.poll_events()
-#     gl.glClear(int(gl.GL_COLOR_BUFFER_BIT) | int(gl.GL_DEPTH_BUFFER_BIT))
-#     renderer.new_frame()
-#     im.new_frame()
-#     im.push_font(font, 0)
-
-#     viewport = im.get_main_viewport()
-#     im.set_next_window_pos(viewport.pos)
-#     im.set_next_window_size(viewport.size)
-#     im.begin(
-#       "Root",
-#       flags=(
-#         im.WindowFlags.NO_TITLE_BAR
-#         | im.WindowFlags.NO_RESIZE
-#         | im.WindowFlags.NO_MOVE
-#         | im.WindowFlags.NO_COLLAPSE
-#         | im.WindowFlags.NO_BRING_TO_FRONT_ON_FOCUS
-#         | im.WindowFlags.NO_NAV_FOCUS
-#       ),
-#     )
-#     im.end()
-
-#     frame()
-
-#     im.pop_font()
-#     im.render()
-#     renderer.render(im.get_draw_data())
-#     glfw.swap_buffers(window)
-
-#   renderer.shutdown()
-#   im.destroy_context(None)
-#   ##
 
 
 _show_code_states: dict[str, bool] = {}
